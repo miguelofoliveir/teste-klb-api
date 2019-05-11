7b9086d0c2ec70876dcbff6b81dd34ec65130798
miguelofoliveir <miguelofoliveira@gmail.com>
2019-05-11 18:40:54 -0300

    ajuste api

--- index.js
+++ index.js
@@ -4,26 +4,44 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const data = {
-    "actions": [
-        "exibir",
-        "salvar",
-        "deletar"
-    ],
-    "menu": [
-        "Painel de Gestão",
-        "Conta Digital",
-        "Ant. de Recebíveis"
-    ]
-
+  "actions": [{
+      id: 1,
+      description: "exibir"
+    },
+    {
+      id: 2,
+      description: "salvar"
+    },
+    {
+      id: 3,
+      description: "deletar"
+    }
+  ],
+  "menu": [{
+      id: 1,
+      description: "Painel de Gestão"
+    },
+    {
+      id: 2,
+      description: "Conta Digital"
+    },
+    {
+      id: 3,
+      description: "Ant. de Recebíveis"
+    }
+  ]
 }
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
+  res.writeHead(200, {
+    'Content-Type': 'text/plain',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE'
+  });
   res.end(JSON.stringify(data));
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-
